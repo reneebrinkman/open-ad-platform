d83104ac0dc0b9020a75f8b599856d8042a31938
ben@benjaminbrinkman.com <ben@benjaminbrinkman.com>
2015-08-06 06:27:31 -0500

    mysql support is now available

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE_SETTINGS = {
-    'name': '',
-    'user': '',
-    'password': '',
-    'host': '',
-    'port': '',
+    'name': 'open-ad-platform',
+    'user': 'user',
+    'password': 'pass',
+    'host': 'localhost',
+    'port': 3306,
 }
 
-SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}:{}/{}'.format(
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(
     DATABASE_SETTINGS['user'],
     DATABASE_SETTINGS['password'],
     DATABASE_SETTINGS['host'],
     DATABASE_SETTINGS['port'],
-    DATABASE_SETTINGS['name'],
+    DATABASE_SETTINGS['name']
 )
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
