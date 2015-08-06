e8d7a0df2da8a05bcfb33466487fd548d0e677f2
ben@benjaminbrinkman.com <ben@benjaminbrinkman.com>
2015-08-06 08:03:12 -0500

    implemented views and templates for displaying ads and also added more relevant information to the index view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
+from app import models
+import random
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world"
+    return render_template('index.html')
+
+@app.route('/ad')
+def get_ad():
+    ad = random.choice(models.Advertisement.query.all())
+    return render_template('ad.html', **ad.render())
